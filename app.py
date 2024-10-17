1efba2964a1b429aa08611c7b5e5b2e05cf5b81b
misgraal <arturpetrov07@gmail.com>
2024-10-18 00:46:49 +0300

    final2

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 import config
 from eventLib import event
@@ -13,13 +10,6 @@ CHAT_ID = 727148312
 EVENT_FILE = './event.txt'
 
 app = Flask(__name__)
-app.secret_key = 'some_random_secret_key'  # For flash messages
-
-# Configure your email server (this example uses Gmail SMTP)
-SMTP_SERVER = 'smtp.gmail.com'
-SMTP_PORT = 587
-USERNAME = 'arturpetrov07@gmail.com'
-PASSWORD = 'nnsv aybk jhyp xfsy'  # Use app-specific password if using Gmail 2FA
 
 @app.route('/')
 def contact():
@@ -28,7 +18,6 @@ def contact():
 @app.route('/submit_form', methods=['POST'])
 def submit_form():
     if request.method == 'POST':
-        # Get form data
         name = request.form['name']
         email = request.form['email']
         message = request.form['message']
@@ -47,35 +36,20 @@ def submit_form():
 
 
         if recaptcha_result['success'] and recaptcha_result['score'] >= 0.5:
-            msg = MIMEMultipart()
-            msg['From'] = email
-            msg['To'] = USERNAME
-            msg['Subject'] = f"New message from {name}"
 
             # Email body content
             body = f"Name: {name}\enter/Email: {email}\enter/Message:\enter/{message}"
 
             # Send the email
             try:
-                """server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-                server.starttls()
-                server.login(USERNAME, PASSWORD)
-                text = msg.as_string()
-                server.sendmail(email, USERNAME, text)
-                server.quit()"""
-
                 text = body
                 print(text)
                 events.TriggerEvent('sendMessage', text)
-
-                flash("Your message has been sent successfully!", "success")
             except Exception as e:
                 print(f"Error: {e}")
-                flash("There was an issue sending your message. Please try again later.", "danger")
 
             return redirect(url_for('contact'))
         else:
-            flash("reCAPTCHA verification failed. Please try again.", "danger")
             return redirect(url_for('contact'))
 
 if __name__ == '__main__':
--- telegram.py
+++ telegram.py
@@ -4,17 +4,11 @@ from eventLib import event
 
 bot = telebot.TeleBot(config.TELEGRAM_BOT_TOKEN)
 
-
 events = event('event.txt')
-
-
-
-
 
 @bot.message_handler(commands=["start"])
 def start(message):
     bot.send_message(message.chat.id, message.chat.id)
-
 
 def sendForm():
     while True:
@@ -25,12 +19,9 @@ def sendForm():
             bot.send_message("727148312", text)
         
 if __name__ == '__main__':
-    # Use threading to run both polling and sending repetitive messages concurrently
     from threading import Thread
 
-    # Start the polling loop in a separate thread
     polling_thread = Thread(target=bot.polling)
     polling_thread.start()
 
-    # Start the repetitive message sending loop in the main thread or another thread
     sendForm()
